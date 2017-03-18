eab204fa79fb3b70cd871f508da004478d89f9c2
SnowCheng <260732891@qq.com>
2017-03-19 00:35:46 +0800

    MOD : test for ACRouter

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -36,6 +36,15 @@ class Tests: XCTestCase {
         
     }
     
+    func testResponse() {
+        let response = ACRouter.requestURL("AA://bb/hello/dd?query=value1#?newquery=newvalue", userInfo: ["query": "value2" as AnyObject])
+    
+        XCTAssertEqual(response.pattern?.patternString, p4)
+        XCTAssertEqual(response.queries["cc"] as? String, "hello")
+        XCTAssertEqual(response.queries["query"] as! [String], ["value1", "value2"])
+        XCTAssertEqual(response.queries["newquery"]as? String, "newvalue")
+    }
+    
     func testIntercept() {
         var response: ACRouter.RouteResponse
         
@@ -77,6 +86,59 @@ class Tests: XCTestCase {
         
         response = ACRouter.requestURL("AA://bb/helllo/dd?shoudContinue1=false&shoudContinue2=true")
         XCTAssertNil(response.pattern)
+        
+    }
+    
+    func testRequest() {
+        var s = "AA://bb/cc/dd?query=value"
+        var r = ACRouterRequest.init(s)
+        XCTAssertEqual(r.sheme, "AA")
+        XCTAssertEqual(r.paths, ["bb", "cc", "dd"])
+        XCTAssertEqual(r.queries as! [String: String], ["query": "value"])
+        
+        s = "AA://bb/cc/dd?query=value#newPath"
+        r = ACRouterRequest.init(s)
+        XCTAssertEqual(r.sheme, "AA")
+        XCTAssertEqual(r.paths, ["bb", "cc", "dd", "newPath"])
+        XCTAssertEqual(r.queries as! [String: String], ["query": "value"])
+        
+        s = "AA://bb/cc/dd?query=value#?newquery=newvalue"
+        r = ACRouterRequest.init(s)
+        XCTAssertEqual(r.sheme, "AA")
+        XCTAssertEqual(r.paths, ["bb", "cc", "dd"])
+        XCTAssertEqual(r.queries as! [String: String], ["query": "value", "newquery": "newvalue"])
+        
+        s = "AA://bb/cc/dd?query=value#newPath?newquery=newvalue"
+        r = ACRouterRequest.init(s)
+        XCTAssertEqual(r.sheme, "AA")
+        XCTAssertEqual(r.paths, ["bb", "cc", "dd", "newPath"])
+        XCTAssertEqual(r.queries as! [String: String], ["query": "value", "newquery": "newvalue"])
+    }
+    
+    func testPattern() {
+        var s = "AA://bb/:cc/dd"
+        var p = ACRouterPattern.init(s) {_ in return nil}
+        XCTAssertEqual(p.sheme, "AA")
+        XCTAssertEqual(p.patternPaths, ["bb", ":cc", "dd"])
+        XCTAssertEqual(p.paramsMatchDict, ["cc": 1])
+        XCTAssertEqual(p.matchString, "bb/~AC~/dd")
+        
+        s = "AA://bb/:cc/dd?query=value#newPath?newqury=newvalue"
+        p = ACRouterPattern.init(s) {_ in return nil}
+        XCTAssertEqual(p.sheme, "AA")
+        XCTAssertEqual(p.patternPaths, ["bb", ":cc", "dd", "newPath"])
+        XCTAssertEqual(p.paramsMatchDict, ["cc": 1])
+        XCTAssertEqual(p.matchString, "bb/~AC~/dd/newPath")
+    }
+    
+    func testGenerateURL() {
+        let s1 = "AA://bb/:cc/dd"
+        let p1 = ["cc": "value1", "ee": "value2"]
+        let u1 = ACRouter.generate(s1, params: p1, jumpType: .modal)
+        
+        let r1 = ["AA://bb/value1/dd?ACJumpTypeKey=ACJumpTypeModal&ee=value2",
+                  "AA://bb/value1/dd?ee=value2&ACJumpTypeKey=ACJumpTypeModal"]
+        XCTAssert(r1.contains(u1))
         
     }
     
